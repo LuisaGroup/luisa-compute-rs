38a6cdc4c8a64cae3f6d487ac90884b80df8038f
Xiaochun Tong <xtong@uwaterloo.ca>
2022-09-27 11:06:45 -0400

    test good

--- build.rs
+++ build.rs
@@ -1,6 +1,14 @@
-use std::process::Command;
+
 use bindgen;
 
 fn main() {
-
+    let bindings = bindgen::Builder::default()
+        .header("./ext/LuisaCompute/src/api/language.h")
+        .clang_arg("-I./ext/LuisaCompute/src/")
+        .parse_callbacks(Box::new(bindgen::CargoCallbacks))
+        .generate()
+        .expect("Unable to generate bindings");
+    bindings
+        .write_to_file("src/binding.rs")
+        .expect("Couldn't write bindings!");
 }

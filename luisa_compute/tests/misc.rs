210096e5f74d5aaf0fcdb3a4d2ccf9c716b74050
Xiaochun Tong <xtong@uwaterloo.ca>
2023-05-04 15:57:48 -0400

    update submod

--- luisa_compute/tests/misc.rs
+++ luisa_compute/tests/misc.rs
@@ -16,7 +16,6 @@ fn get_device() -> Device {
         unsafe {
             libc::signal(libc::SIGSEGV, _signal_handler as usize);
         }
-        init_logger();
     });
     let ctx = Context::new(current_exe().unwrap());
     let device = match std::env::var("LUISA_TEST_DEVICE") {

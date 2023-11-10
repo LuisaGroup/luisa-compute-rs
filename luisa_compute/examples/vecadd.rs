789a6a006aa93d4d3c6f777bac7bd78bbc1a02a1
Xiaochun Tong <xtong@uwaterloo.ca>
2023-11-10 03:04:12 -0500

    update submod

--- luisa_compute/examples/vecadd.rs
+++ luisa_compute/examples/vecadd.rs
@@ -1,6 +1,6 @@
-use std::env::current_exe;
 use luisa::prelude::*;
 use luisa_compute as luisa;
+use std::env::current_exe;
 #[tracked]
 fn main() {
     luisa::init_logger();
@@ -23,17 +23,24 @@ fn main() {
     x.view(..).fill_fn(|i| i as f32);
     y.view(..).fill_fn(|i| 1000.0 * i as f32);
 
-    let kernel = device.create_kernel::<fn(Buffer<f32>)>(&|buf_z| {
-        // z is pass by arg
-        let buf_x = x.var(); // x and y are captured
-        let buf_y = y.var();
-        let tid = dispatch_id().x;
-        let x = buf_x.read(tid);
-        let y = buf_y.read(tid);
-        let vx = 2.0_f32.var(); // create a local mutable variable
-        *vx += x; // store to vx
-        buf_z.write(tid, vx + y);
-    });
+    let kernel = device.create_kernel_with_options::<fn(Buffer<f32>)>(
+        KernelBuildOptions {
+            time_trace: true,
+            name: Some("vecadd".into()),
+            ..Default::default()
+        },
+        &|buf_z| {
+            // z is pass by arg
+            let buf_x = x.var(); // x and y are captured
+            let buf_y = y.var();
+            let tid = dispatch_id().x;
+            let x = buf_x.read(tid);
+            let y = buf_y.read(tid);
+            let vx = 2.0_f32.var(); // create a local mutable variable
+            *vx += x; // store to vx
+            buf_z.write(tid, vx + y);
+        },
+    );
     kernel.dispatch([1024, 1, 1], &z);
     let z_data = z.view(..).copy_to_vec();
     println!("{:?}", &z_data[0..16]);
